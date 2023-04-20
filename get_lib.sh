9ed1b2f7b0f9f55055a1fb00bd24b440487f9110
amartinsmg <andremindurimg@gmail.com>
2023-04-20 17:51:47 -0300

    Remove get_lib.sh

--- get_lib.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-[ -d lib ] || mkdir lib
-cd lib
-([ -d include ] && [ -f statistics.h ] && [ -f percentage.h ]) || git clone https://github.com/amartinsmg/MathAlgorithms.git math
-cp  math/src/include ./ -r
-cp math/src/statistics.h math/src/basic_operations.h math/src/percentage.h ./
-rm -rf math
