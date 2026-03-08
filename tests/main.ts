7f6cd5c04fc9a1fce672687b41d8d0eb278b35ea
André Martins <andremindurimg@gmail.com>
2026-03-08 11:52:49 -0300

    Refactor tests/main.ts to improve readability and maintenance

--- tests/main.ts
+++ tests/main.ts
@@ -1,26 +1,32 @@
 import { trimmedMean } from "../src/ts/trimmed_mean";
 
 /**
-  Throws an error if the condition is false
-  @param condition - A boolean expression to be evaluated
-  @throws - Error if the condition is false 
+ Rounds a number to a specified number of decimal places
+ @param num - The number to be rounded
+ @param decimalPlaces - The number of decimal places to round to
+ @return - The rounded number
  */
-
-function assert(condition: any) {
-  if (!condition) throw new Error("Assertion failed");
-}
-
-/**
-  Rounds a number to a specified number of decimal places
-  @param num - The number to be rounded
-  @param decimalPlaces - The number of decimal places to round to
-  @return - The rounded number
-  */
 
 function roundTo(num: number, decimalPlaces: number): number {
   let base10 = 10 ** decimalPlaces,
     result = Math.round(num * base10) / base10;
   return result;
+}
+
+/**
+ * Assert that two numbers are equal within a precision of 6 decimal places.
+ * * @param actual - The numerical value produced by the system.
+ * @param expected - The reference value to compare against.
+ * * @remarks
+ * The function uses `roundTo(n, 6)` to mitigate floating-point precision issues
+ * before performing the equality check.
+ * * @throws {Error} If the rounded `actual` value does not strictly equal `expected`.
+ */
+
+function check(actual: number, expected: number) {
+  const condition = roundTo(actual, 6) == expected;
+  if (!condition)
+    throw new Error(`Test failed: Expected: ${expected}, received: ${actual}`);
 }
 
 /**
@@ -30,11 +36,16 @@ function roundTo(num: number, decimalPlaces: number): number {
  */
 
 void (function () {
-  const Arr = [-10, 2, 2, 2, 2, 2, 3, 3, 4, 4, 5, 5, 6, 7, 7, 8, 8, 8, 10, 11];
-  assert(roundTo(trimmedMean(Arr, 5), 6) == 4.888889);
-  assert(roundTo(trimmedMean(Arr, 10), 6) == 4.75);
-  assert(roundTo(trimmedMean(Arr, 15), 6) == 4.714286);
-  assert(roundTo(trimmedMean(Arr, 20), 6) == 4.666667);
-  assert(roundTo(trimmedMean(Arr, 25), 6) == 4.6);
-  console.log("Passed all tests successfully!");
+  const Arr = [-10, 2, 2, 2, 2, 2, 3, 3, 4, 4, 5, 5, 6, 7, 7, 8, 8, 8, 10, 1000];
+  try {
+    check(trimmedMean(Arr, 5), 4.888889);
+    check(trimmedMean(Arr, 10), 4.75);
+    check(trimmedMean(Arr, 15), 4.714286);
+    check(trimmedMean(Arr, 20), 4.666667);
+    check(trimmedMean(Arr, 25), 4.6);
+    console.log("Passed all tests successfully!");
+  } catch (e) {
+    if (e instanceof Error) console.log(e.message);
+    else console.log(e);
+  }
 })();
